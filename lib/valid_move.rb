066a04e3e416a7e1d0067e78bcf920186e6630d5
Jabe Rooney <abrahamsahlzghi@gmail.com>
2019-02-17 19:08:02 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,10 +6,30 @@
 book1 = "sorcer"
 book2 = "chamber"
 book3 = "prisoner"
-books = [book1,book2,book3]
+books = [book1,book2,book3,"goblets of fire", "order of the phoenix","half blood prince","deathly hallows pt.1"]
 
-def print_titles(books)
-  puts books[0]
-end
+#loop over the titles
+#build an array of the full titles of the books
+#harry potter and the 
 
-print_titles(books)
+
+
+
+
+
+
+
+
+
+
+
+#def print_titles(books)
+ #use .times and the length of the 
+ 
+# books.count.times do |step|
+#times "use .times do |count| and each ".each do" #|titles|
+#the step could be changed to what ever we want it to #say.
+#  puts books[step]
+#end
+
+#print_titles(books)
