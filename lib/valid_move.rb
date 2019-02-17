bf07d0bd84f167910d9fc48be71822a3b08c8ae2
Jabe Rooney <abrahamsahlzghi@gmail.com>
2019-02-17 19:13:51 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,7 +12,7 @@ books = [book1,book2,book3,"goblets of fire", "order of the phoenix","half blood
 #build an array of the full titles of the books
 #harry potter and the -
 
-booktitles = [sorcer,chamber,prisoner]
+booktitles = [title1,title2,title3,title4,title5,title6,title7]
 
 
 
