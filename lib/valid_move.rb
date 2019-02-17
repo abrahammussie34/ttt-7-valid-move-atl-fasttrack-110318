9d024db94ee75d44455b0c553d1220d1a01253b6
Jabe Rooney <abrahamsahlzghi@gmail.com>
2019-02-17 18:50:12 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,3 +3,13 @@
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
+book1 = "sorcer"
+book2 = "chamber"
+book3 = "prisoner"
+books = [book1,book2,book3]
+
+def print_titles(books)
+  puts books[0]
+end
+
+print_titles(books)
